ae98571427d97443e7e3da982053e3e63a8f2918
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2024-04-27 18:50:57 -0300

    First Commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import  {FaUser, FaLock } from "react-icons/fa";
+import { useState } from 'react';
+import "./Login.Style.css";
+
+const Login = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("Enviando os dados: " + userName + " - " + password)
+    }
+
+  return (
+    <div className="Container">
+        <form onSubmit={handleSubmit}>
+            <h1>Acesse o Sistema</h1>
+            <div>
+                <input 
+                type="email" 
+                placeholder='E-mail' 
+                onChange={(e) => setUserName(e.target.value)}
+                />
+                <FaUser className='icon'></FaUser>
+            </div>
+            <div>
+                <input 
+                type="password" 
+                placeholder='Senha' 
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <FaLock className='icon'></FaLock>
+            </div>
+
+            <div className="recall-forget">
+                <label>
+                    <input type="checkbox" />
+                    Lembre-se de mim
+                </label>
+                <a href="#">Esqueci minha senha</a>
+            </div>
+            <button>Entrar</button>
+
+            <div className="signup-link">
+                <p>Não tem uma conta? <a href='#'> Registre-se</a></p>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
